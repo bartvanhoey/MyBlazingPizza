4e5673bce1154e98b45ff9723c19845a10253c0e
bartvanhoey <bartvanhoey@hotmail.com>
2020-07-24 20:43:25 +0200

    end-01-components-and-layout

--- BlazingPizza.Shared/Topping.cs
+++ BlazingPizza.Shared/Topping.cs
@@ -5,6 +5,6 @@ namespace BlazingPizza.Shared
     public int Id { get; set; }
     public string Name { get; set; }
     public decimal Price { get; set; }
-    public string GetFormattedPrice() => Price.ToString("0:00");
+    public string GetFormattedPrice() => Price.ToString("0.00");
   }
 }
