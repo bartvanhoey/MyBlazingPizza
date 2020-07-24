793392ff6bccc75e34f171a0748c2959d9bf90ff
bartvanhoey <bartvanhoey@hotmail.com>
2020-07-24 17:15:30 +0200

    setup dbcontext

---
+++ BlazingPizza.Server/PizzaStoreContext.cs
@@ -0,0 +1,36 @@
+using BlazingPizza.Shared;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BlazingPizza.Server
+{
+  public class PizzaStoreContext : ApiAuthorizationDbContext<PizzaStoreUser>
+  {
+    public PizzaStoreContext(DbContextOptions options, 
+    IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+    {
+    }
+
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Pizza> Pizzas { get; set; }
+    public DbSet<PizzaSpecial> Specials { get; set; }
+    public DbSet<Topping> Toppings { get; set; }
+    public DbSet<NotificationSubscription> NotificationSubscriptions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity<PizzaTopping>().HasKey(pst => new { pst.PizzaId, pst.ToppingId });
+      modelBuilder.Entity<PizzaTopping>().HasOne<Pizza>().WithMany(ps => ps.Toppings);
+      modelBuilder.Entity<PizzaTopping>().HasOne(pst => pst.Topping).WithMany();
+
+      modelBuilder.Entity<Order>().OwnsOne(o => o.DeliveryLocation);
+
+
+    }
+
+  }
+}
--- BlazingPizza.Server/Program.cs
+++ BlazingPizza.Server/Program.cs
@@ -1,26 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace BlazingPizza.Server
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+      var host = CreateHostBuilder(args).Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      var scopeFactory = host.Services.GetRequiredService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope())
+      {
+        var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+        if (db.Database.EnsureCreated())
+        {
+          SeedData.Initialize(db);
+        }
+      }
+      host.Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
